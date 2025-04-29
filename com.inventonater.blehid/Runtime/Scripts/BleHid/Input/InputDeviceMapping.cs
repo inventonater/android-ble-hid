88b1097e6804bc0ed53000864ea03f97fd933984
naschenbach <naschenbach@fb.com>
2025-04-29 00:10:17 -0700

    Update InputDeviceMapping.cs

--- com.inventonater.blehid/Runtime/Scripts/BleHid/Input/InputDeviceMapping.cs
+++ com.inventonater.blehid/Runtime/Scripts/BleHid/Input/InputDeviceMapping.cs
@@ -37,27 +37,31 @@ namespace Inventonater.BleHid
 
         public string Name { get; }
 
-        public void Add(InputEvent.Id id, InputEvent.Phase buttonPhase, Action action) => Add(new InputEvent(id, buttonPhase), action);
-        public void Add(InputEvent buttonEvent, Action action) => _buttonMapping.AppendValue(buttonEvent, action);
-        public void Add(EInputEvent buttonEvent, Action action) => _buttonMapping.AppendValue(buttonEvent.ToInputEvent(), action);
-        public void Add(EInputEvent e, EInputAction a) => Add(e, _actionRegistry.GetAction(a));
-        public void AddPressRelease(InputEvent.Id button, EInputAction press, EInputAction release)
-        {
-            Add(button, InputEvent.Phase.Press, _actionRegistry.GetAction(press));
-            Add(button, InputEvent.Phase.Release, _actionRegistry.GetAction(release));
-        }
+        public void Add(EInputEvent e, EInputAction a) => _buttonMapping.AppendValue(e.ToInputEvent(), _actionRegistry.GetAction(a));
         public void Add(IAxisMapping axisMapping) => _axisMappings.Add(axisMapping);
+
+        public static InputDeviceMapping Create(string name, ActionRegistry registry, List<(EInputEvent, EInputAction)> map)
+        {
+            var inputDeviceMapping = new InputDeviceMapping(name, registry);
+            foreach (var entry in map) inputDeviceMapping.Add(entry.Item1, entry.Item2);
+            return inputDeviceMapping;
+        }
 
         public static InputDeviceMapping BleMouse(ActionRegistry registry)
         {
-            var mapping = new InputDeviceMapping("BleMouse", registry);
-            mapping.AddPressRelease(InputEvent.Id.Primary, EInputAction.MouseLeftPress, EInputAction.MouseLeftRelease);
-            mapping.AddPressRelease(InputEvent.Id.Secondary, EInputAction.MouseRightPress, EInputAction.MouseRightRelease);
-            mapping.Add(Up, EInputAction.KeyboardArrowUp);
-            mapping.Add(Right, EInputAction.KeyboardArrowRight);
-            mapping.Add(Down, EInputAction.KeyboardArrowDown);
-            mapping.Add(Left, EInputAction.KeyboardArrowLeft);
+            var map = new List<(EInputEvent, EInputAction)>
+            {
+                (PrimaryPress, EInputAction.MouseLeftPress),
+                (PrimaryRelease, EInputAction.MouseLeftRelease),
+                (SecondaryPress, EInputAction.MouseRightPress),
+                (SecondaryRelease, EInputAction.MouseRightRelease),
+                (Up, EInputAction.KeyboardArrowUp),
+                (Right, EInputAction.KeyboardArrowRight),
+                (Down, EInputAction.KeyboardArrowDown),
+                (Left, EInputAction.KeyboardArrowLeft),
+            };
 
+            var mapping = Create("BleMouse", registry, map);
             mapping.Add(new MousePositionAxisMapping(registry.GetMouseMoveAction()));
             mapping.Add(new SingleIncrementalAxisMapping(Axis.Z, registry.GetAction(EInputAction.MediaVolumeUp), registry.GetAction(EInputAction.MediaVolumeDown)));
             return mapping;
@@ -65,44 +69,51 @@ namespace Inventonater.BleHid
 
         public static InputDeviceMapping BleMedia(ActionRegistry registry)
         {
-            var mapping = new InputDeviceMapping("BleMedia", registry);
+            var map = new List<(EInputEvent, EInputAction)>
+            {
+                (PrimaryDoubleTap, EInputAction.MediaPlayPause),
+                (Right, EInputAction.MediaNextTrack),
+                (Left, EInputAction.MediaPreviousTrack),
+                (Up, EInputAction.MediaMute),
+                (Down, EInputAction.MediaMute),
+            };
 
-            mapping.Add(PrimaryDoubleTap, EInputAction.MediaPlayPause);
-            mapping.Add(Right, EInputAction.MediaNextTrack);
-            mapping.Add(Left, EInputAction.MediaPreviousTrack);
-            mapping.Add(Up, EInputAction.MediaMute);
-            mapping.Add(Down, EInputAction.MediaMute);
-
+            var mapping = Create("BleMedia", registry, map);
             mapping.Add(new SingleIncrementalAxisMapping(Axis.Z, registry.GetAction(EInputAction.MediaVolumeUp), registry.GetAction(EInputAction.MediaVolumeDown)));
             return mapping;
         }
 
+
         public static InputDeviceMapping LocalMedia(ActionRegistry registry)
         {
-            var mapping = new InputDeviceMapping("LocalMedia", registry);
+            var map = new List<(EInputEvent, EInputAction)>
+            {
+                (PrimaryDoubleTap, EInputAction.LocalPlayPause),
+                (Right, EInputAction.LocalNextTrack),
+                (Left, EInputAction.LocalPreviousTrack),
+                (Up, EInputAction.LocalMute),
+                (Down, EInputAction.LocalMute),
+            };
 
-            mapping.Add(PrimaryDoubleTap, EInputAction.LocalPlayPause);
-            mapping.Add(Right, EInputAction.LocalNextTrack);
-            mapping.Add(Left, EInputAction.LocalPreviousTrack);
-            mapping.Add(Up, EInputAction.LocalMute);
-            mapping.Add(Down, EInputAction.LocalMute);
-
+            var mapping = Create("LocalMedia", registry, map);
             mapping.Add(new SingleIncrementalAxisMapping(Axis.Z, registry.GetAction(EInputAction.LocalVolumeUp), registry.GetAction(EInputAction.LocalVolumeDown)));
             return mapping;
         }
 
         public static InputDeviceMapping LocalDPad(ActionRegistry registry)
         {
-            var mapping = new InputDeviceMapping("LocalDPad", registry);
+            var map = new List<(EInputEvent, EInputAction)>
+            {
+                (PrimaryTap, EInputAction.LocalDPadCenter),
+                (SecondaryTap, EInputAction.LocalBack),
+                (SecondaryDoubleTap, EInputAction.LocalHome),
+                (Up, EInputAction.LocalDPadUp),
+                (Right, EInputAction.LocalDPadRight),
+                (Down, EInputAction.LocalDPadDown),
+                (Left, EInputAction.LocalDPadLeft),
+            };
 
-            mapping.Add(PrimaryTap, EInputAction.LocalDPadCenter);
-            mapping.Add(SecondaryTap, EInputAction.LocalBack);
-            mapping.Add(SecondaryDoubleTap, EInputAction.LocalHome);
-            mapping.Add(Up, EInputAction.LocalDPadUp);
-            mapping.Add(Right, EInputAction.LocalDPadRight);
-            mapping.Add(Down, EInputAction.LocalDPadDown);
-            mapping.Add(Left, EInputAction.LocalDPadLeft);
-
+            var mapping = Create("LocalDPad", registry, map);
             mapping.Add(new SingleIncrementalAxisMapping(Axis.Z, registry.GetAction(EInputAction.LocalVolumeUp), registry.GetAction(EInputAction.LocalVolumeDown)));
             return mapping;
         }
@@ -111,18 +122,17 @@ namespace Inventonater.BleHid
         private static readonly Vector2 Pixel9XLResolution = new Vector2(1344, 2992);
         private static Vector2 Resolution => Pixel9XLResolution;
         private static Vector2 ClampToScreen(Vector2 vector2) => new(Mathf.Clamp(vector2.x, 0, Resolution.x), Mathf.Clamp(vector2.y, 0, Resolution.y));
+
         private static Vector2 ScreenCenter() => Resolution * 0.5f;
-        public static InputDeviceMapping LocalDrag(BleBridge bridge)
-        {
-            var serviceBridge = bridge.AccessibilityService;
-            var mapping = new InputDeviceMapping("LocalDrag", bridge.ActionRegistry);
-
-            var swipeMapping = new MousePositionAxisMapping(serviceBridge.SwipeExtend, requirePress: true);
-            mapping.Add(PrimaryPress, () => serviceBridge.SwipeBegin(ScreenCenter()));
-            mapping.Add(PrimaryRelease, () => serviceBridge.SwipeEnd());
-            mapping.Add(swipeMapping);
-
-            return mapping;
-        }
+        // public static InputDeviceMapping LocalDrag(BleBridge bridge)
+        // {
+        //     var serviceBridge = bridge.AccessibilityService;
+        //     var mapping = new InputDeviceMapping("LocalDrag", bridge.ActionRegistry);
+        //     var swipeMapping = new MousePositionAxisMapping(serviceBridge.SwipeExtend, requirePress: true);
+        //     mapping.Add(PrimaryPress, () => serviceBridge.SwipeBegin(ScreenCenter()));
+        //     mapping.Add(PrimaryRelease, () => serviceBridge.SwipeEnd());
+        //     mapping.Add(swipeMapping);
+        //     return mapping;
+        // }
     }
 }
